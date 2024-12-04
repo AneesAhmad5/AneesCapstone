0b14ee32627f8b27aed423d38e036c2a9bfe993f
AneesAhmad5 <6800588@spsd.sk.ca>
2024-12-04 15:20:30 -0600

    Finished the transitions and menu. Started on the game setup.
    Finished the terrain, starting the cannons tomorrow.

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -6,17 +6,25 @@
 
 //broadcasting statements that dictate what stage the game is at. 
 let menu_screen = true;
-let transition = false;
+let transitionBlack = false;
+let transitionColour = false;
 let countdown = false;
 let game_start = false;
 let normal_mode = false;
 
+
+
+
+
 //setting a colour for the background that will change.
 let backgroundRed = 100;
-let backgroundGreen = 0;
-let backgroundBlue = 0;
+let backgroundGreen = 10;
+let backgroundBlue = 10;
 let BlackScreen = false;
 
+
+
+// WEBSITE FUNCTIONS
 
 function preload(){ 
   //loading fonts for the title
@@ -32,16 +40,17 @@ function draw() {
     menu();
   }
   //transition
-  else if (transition === true){
-    if(BlackScreen === false){
-      fadeOut();
-    }
-    if(BlackScreen !== true){ // FIX THIS <-----------------------------------------------------------------------
-      fadeOut;
-    }
+  else if (transitionBlack === true){
+    fadeOut();
     print(backgroundRed, backgroundGreen, backgroundBlue);
-    transition = false;
-    BlackScreen = false;
+    if(backgroundRed === 0 && backgroundGreen === 0 && backgroundBlue ===0){ //if the colour is black, stop transitioning.
+      transitionBlack = false;
+      transitionColour = true;
+    }
+  }
+  else if (transitionColour === true){ //transitioning to the colour i need
+    fadeIn(200, 196, 255);
+    print(backgroundRed, backgroundGreen, backgroundBlue);
   }
   //game page
   else if(game_start === true){
@@ -50,9 +59,18 @@ function draw() {
   else{ //if nothing is received, just make a black screen. 
     background(0);
   }
+
 }
 
-//creating the menu
+
+
+
+
+
+
+
+
+//                MENU                 \\
 function menu(){
   background(backgroundRed, backgroundGreen, backgroundBlue); //setting the background.
   //the title
@@ -89,17 +107,20 @@ function play_button(){
 function mouseClicked(){
   if(menu_screen === true && mouseY < windowHeight/2+350 && mouseY > windowHeight/2+50 && mouseX > windowWidth/2-400 && mouseX < windowWidth/2+400 ) {//if mouse is hovering over the play button
     menu_screen = false;
-    transition = true;
+    transitionBlack = true;
+    game_start = true;
     print(menu_screen);
   }
 }
 
 
+//    TRANSITIONING    \\
+
 //adding a fading transition that can be applied
 function fadeOut(){
   //fade into black
   background(backgroundRed, backgroundGreen, backgroundBlue);
-  if(backgroundRed > 0){
+  if(backgroundRed > 0){ 
     backgroundRed -= 1; 
   }
   if(backgroundBlue > 0){
@@ -111,17 +132,65 @@ function fadeOut(){
 }
 
 function fadeIn(r, g, b){
+  background(backgroundRed, backgroundGreen, backgroundBlue);
+  //fade into specfied colour
   if(backgroundRed < r){
     backgroundRed += 1; 
   }
-  if(backgroundBlue > b){
+  if(backgroundBlue < b){
     backgroundBlue += 1;
   }
-  if(backgroundGreen > g){
+  if(backgroundGreen < g){
     backgroundGreen += 1;
+  }
+  if(backgroundRed === r && backgroundGreen === g && backgroundBlue ===b){ //once the colour has been applied, stop. 
+    transitionColour = false;
   }
 }
 
+
+
+//        GAME SETUP        \\
 function game_background(){
   background(backgroundRed, backgroundGreen, backgroundBlue); //setting the background.
+  createTerrain();
+  
 }
+
+//TERRAIN GENERATING
+//setting variables for the terrain 
+let terrainUnitWidth = 10; //width of each terrain unit
+let interval = 0.01; //the difference in unit heights
+function createTerrain(){
+  let time = 0;
+  for(let x = 0; x < width; x += terrainUnitWidth){
+    let y = noise(time) * height;
+    noStroke();
+    rect(x, height, terrainUnitWidth, -1*y);
+    time += interval;  
+  }
+}
+
+//CANNON
+//player 1
+let cannonX = (windowWidth * 0.25);
+let cannonY = 500;
+//player2
+let cannonX2 = (windowWidth * 0.75);
+let cannonY2 = 500;
+
+class Cannon{
+  constructor(x,y,direction){
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  }
+  display(){
+    rectMode(CENTER);
+    stroke(0);
+    fill(122, 63, 0);
+
+  }
+
+} 
+
