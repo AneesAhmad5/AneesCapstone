80f1f30eb95526d0c115fff8d9e895e208544b37
AneesAhmad5 <6800588@spsd.sk.ca>
2024-12-09 15:20:31 -0600

    Started to make the cannon heads move.
    will finish the rest next day.

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -27,7 +27,7 @@ let BlackScreen = false;
 
 // WEBSITE FUNCTIONS
 
-function preload(){ 
+function preload() {
   //loading fonts for the title
   titlefont = loadFont("assets/GAMEDAY.ttf");
 }
@@ -37,38 +37,43 @@ function setup() {
   //setting cannon positions. Must be done before creating new cannons and has to be within a system. 
   cannonX = windowWidth * 0.25;
   cannonX2 = windowWidth * 0.75;
-  cannonY = windowHeight *0.75;
+  cannonY = windowHeight * 0.75;
   cannonY2 = windowHeight * 0.75;
-  player1 = new Cannon(cannonX, cannonY, mouseX);
-
-  player2 = new Cannon(cannonX2, cannonY2, mouseX);
+  player1 = new Cannon(cannonX, cannonY, 0);
+  angleMode(DEGREES);
+  player2 = new Cannon(cannonX2, cannonY2, 0);
 }
 
 function draw() {
-  if(menu_screen === true){ //checking for menu broadcast
+  if (menu_screen === true) { //checking for menu broadcast
     menu();
   }
   //transition
-  else if (transitionBlack === true){
+  else if (transitionBlack === true) {
     fadeOut();
     print(backgroundRed, backgroundGreen, backgroundBlue);
-    if(backgroundRed === 0 && backgroundGreen === 0 && backgroundBlue ===0){ //if the colour is black, stop transitioning.
+    if (backgroundRed === 0 && backgroundGreen === 0 && backgroundBlue === 0) { //if the colour is black, stop transitioning.
       transitionBlack = false;
       transitionColour = true;
     }
   }
-  else if (transitionColour === true){ //transitioning to the colour i need
+  else if (transitionColour === true) { //transitioning to the colour i need
     fadeIn(200, 196, 255);
     print(backgroundRed, backgroundGreen, backgroundBlue);
   }
   //game page
-  else if(game_start === true){
+  else if (game_start === true) {
     game_background();
   }
-  else{ //if nothing is received, just make a black screen. 
+  else { //if nothing is received, just make a black screen. 
     background(0);
   }
-
+  if (keyIsDown(RIGHT_ARROW) === true) {
+    player2.direction += 5;
+  }
+  if(keyIsDown(LEFT_ARROW) === true){
+    player2.direction -= 5;
+  }
 }
 
 
@@ -80,14 +85,14 @@ function draw() {
 
 
 //                MENU                 \\
-function menu(){
+function menu() {
   background(backgroundRed, backgroundGreen, backgroundBlue); //setting the background.
   //the title
   fill(0);
   textAlign(CENTER);
   textFont(titlefont); //new font
-  textSize(200); 
-  text("Cannon Game", windowWidth/2, windowHeight/2-100);
+  textSize(200);
+  text("Cannon Game", windowWidth / 2, windowHeight / 2 - 100);
   //adding a play button as a separate function to clean this up
   play_button();
 }
@@ -95,26 +100,26 @@ function menu(){
 let playcolour = (69, 140, 81); //play text colour
 let buttonColour = (40, 65, 158); //play button colour
 //play button
-function play_button(){ 
+function play_button() {
   fill(buttonColour);
   rectMode(CENTER);
-  rect(windowWidth/2, windowHeight/2+200, 800, 300);
+  rect(windowWidth / 2, windowHeight / 2 + 200, 800, 300);
   fill(playcolour);
   textSize(100);
-  text("PLAY", windowWidth/2, windowHeight/2+225); // play button
-  if(mouseY < windowHeight/2+350 && mouseY > windowHeight/2+50 && mouseX > windowWidth/2-400 && mouseX < windowWidth/2+400){ //mouse hover function - changes colour
+  text("PLAY", windowWidth / 2, windowHeight / 2 + 225); // play button
+  if (mouseY < windowHeight / 2 + 350 && mouseY > windowHeight / 2 + 50 && mouseX > windowWidth / 2 - 400 && mouseX < windowWidth / 2 + 400) { //mouse hover function - changes colour
     buttonColour = color(28, 53, 97); //second colours
     playcolour = color(36, 74, 43);
   }
-  else{
+  else {
     buttonColour = color(40, 65, 158); // base colours
     playcolour = color(69, 140, 81);
   }
 }
 
 //all the mouse clicking functions will be here.
-function mouseClicked(){
-  if(menu_screen === true && mouseY < windowHeight/2+350 && mouseY > windowHeight/2+50 && mouseX > windowWidth/2-400 && mouseX < windowWidth/2+400 ) {//if mouse is hovering over the play button
+function mouseClicked() {
+  if (menu_screen === true && mouseY < windowHeight / 2 + 350 && mouseY > windowHeight / 2 + 50 && mouseX > windowWidth / 2 - 400 && mouseX < windowWidth / 2 + 400) {//if mouse is hovering over the play button
     menu_screen = false;
     transitionBlack = true;
     game_start = true;
@@ -126,33 +131,33 @@ function mouseClicked(){
 //    TRANSITIONING    \\
 
 //adding a fading transition that can be applied
-function fadeOut(){
+function fadeOut() {
   //fade into black
   background(backgroundRed, backgroundGreen, backgroundBlue);
-  if(backgroundRed > 0){ 
-    backgroundRed -= 1; 
+  if (backgroundRed > 0) {
+    backgroundRed -= 1;
   }
-  if(backgroundBlue > 0){
+  if (backgroundBlue > 0) {
     backgroundBlue -= 1;
   }
-  if(backgroundGreen > 0){
+  if (backgroundGreen > 0) {
     backgroundGreen -= 1;
   }
 }
 
-function fadeIn(r, g, b){
+function fadeIn(r, g, b) {
   background(backgroundRed, backgroundGreen, backgroundBlue);
   //fade into specfied colour
-  if(backgroundRed < r){
-    backgroundRed += 1; 
+  if (backgroundRed < r) {
+    backgroundRed += 1;
   }
-  if(backgroundBlue < b){
+  if (backgroundBlue < b) {
     backgroundBlue += 1;
   }
-  if(backgroundGreen < g){
+  if (backgroundGreen < g) {
     backgroundGreen += 1;
   }
-  if(backgroundRed === r && backgroundGreen === g && backgroundBlue ===b){ //once the colour has been applied, stop. 
+  if (backgroundRed === r && backgroundGreen === g && backgroundBlue === b) { //once the colour has been applied, stop. 
     transitionColour = false;
   }
 }
@@ -160,25 +165,27 @@ function fadeIn(r, g, b){
 
 
 //        GAME SETUP        \\
-function game_background(){
+function game_background() {
   background(backgroundRed, backgroundGreen, backgroundBlue); //setting the background.
   createTerrain();
   player1.display();
   player2.display();
 }
 
+
+
 //TERRAIN GENERATING
 //setting variables for the terrain 
 let terrainUnitWidth = 10; //width of each terrain unit
 let interval = 0.01; //the difference in unit heights
-function createTerrain(){
+function createTerrain() {
   let time = 0;
-  for(let x = 0; x < width; x += terrainUnitWidth){
+  for (let x = 0; x < width; x += terrainUnitWidth) {
     let y = noise(time) * height;
     noStroke();
-    fill(backgroundRed*0.5, backgroundGreen *0.5, backgroundBlue*0.5);
-    rect(x, height, terrainUnitWidth, -1*y);
-    time += interval;  
+    fill(backgroundRed * 0.5, backgroundGreen * 0.5, backgroundBlue * 0.5);
+    rect(x, height, terrainUnitWidth, -1 * y);
+    time += interval;
   }
 }
 
@@ -191,26 +198,52 @@ let cannonX2;
 let cannonY2;
 
 
-class Cannon{
-  constructor(x,y,direction){
+class Cannon {
+  constructor(x, y, direction) {
     this.x = x;
     this.y = y;
     this.direction = direction;
   }
-  display(){
-    stroke(0);
-    fill(122, 63, 0);
+  display() {
+    noStroke();
+    fill(31, 26, 26);
+    //cannon barrel
+    //player 2
+    if (this.x > windowWidth / 2) {
+      //rotating the canon
+      push();
+      translate(this.x, this.y);
+      rotate(this.direction);
+      rect(-25, 0, 70, 20);
+      pop();
+    }
+    //player 1
+    else {
+      push();
+      translate(this.x, this.y);
+      rotate(this.direction);
+      rect(25, 0, 70, 20);
+      pop();
+    }
+    fill(31, 26, 26);
+    //cannon head
     circle(this.x, this.y, 50);
     rectMode(CENTER);
-    fill(99, 95, 91);
-    if(this.x > windowWidth/2){
-      rect(this.x-25, this.y, 70, 20);
-    }
-    else{
-      rect(this.x+25, this.y, 70, 20);
-    }
-    rect(this.x, this.y+25, 50, 20);
+    //wheels
+    fill(0);
+    circle(this.x - 25, this.y + 35, 20);
+    circle(this.x + 25, this.y + 35, 20);
+    //cannon base
+    fill(122, 63, 0);
+    rect(this.x, this.y + 25, 70, 20);
+
+
   }
 
-} 
+}
 
+class Projectile {
+  constructor(x, y, direction, speed, type,) {
+
+  }
+}
